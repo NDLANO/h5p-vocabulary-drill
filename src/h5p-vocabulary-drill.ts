f986cc7e45689faf27c351802d94ad6859e5df62
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-01-13 10:27:21 +0100

    feat: init drag text or fill in based on params

--- src/h5p-vocabulary-drill.ts
+++ src/h5p-vocabulary-drill.ts
@@ -1,12 +1,46 @@
-import type { H5PFieldText, IH5PContentType } from "h5p-types";
+import type { IH5PContentType } from "h5p-types";
 import { H5P, H5PContentType, registerContentType } from "h5p-utils";
 
-class VocabularyDrill extends H5PContentType<H5PFieldText> implements IH5PContentType {
-  attach($wrapper: JQuery<HTMLElement>) {
-    const wrapper = $wrapper.get(0);
+type Params = {
+  answerMode: "fillIn" | "dragText";
+  enableSwitchAnswerModeButton: boolean;
+};
+
+class VocabularyDrill
+  extends H5PContentType<Params>
+  implements IH5PContentType<Params>
+{
+  attach() {
+    const { contentId } = this;
+    const { answerMode } = this.params;
 
     const dragTextContentType = (H5P as any).DragText;
     const fillInTheBlanksContentType = (H5P as any).Blanks;
+
+    switch (answerMode) {
+      case "dragText": {
+        H5P.newRunnable(
+          dragTextContentType,
+          contentId,
+          H5P.jQuery(this.wrapper),
+        );
+
+        break;
+      }
+
+      case "fillIn":
+      default: {
+        fillInTheBlanksContentType.params = {};
+
+        H5P.newRunnable(
+          fillInTheBlanksContentType,
+          contentId,
+          H5P.jQuery(this.wrapper),
+        );
+
+        break;
+      }
+    }
   }
 }
 
