733ac08a1e8ae3f3bb02a71051d292fa5431e104
Sindre Bøyum <boyum@users.noreply.github.com>
2023-01-13 18:12:57 +0100

    Update src/utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -10,19 +10,9 @@ export const findLibraryInfo = (
 ):
   | Pick<Library, "machineName" | "majorVersion" | "minorVersion">
   | undefined => {
-  const { machineName, majorVersion, minorVersion } =
-    library.preloadedDependencies.find(
+return library.preloadedDependencies.find(
       library => library.machineName === libraryName,
-    ) ?? {};
-  if (isNil(machineName) || isNil(majorVersion) || isNil(minorVersion)) {
-    return undefined;
-  }
-
-  return {
-    machineName,
-    majorVersion,
-    minorVersion,
-  };
+    );
 };
 
 export const libraryToString = ({
