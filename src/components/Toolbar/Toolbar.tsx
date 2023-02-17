c07f7c0627a708338883a80102e3a88d6bab34b7
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2023-02-17 10:51:02 +0100

    feat: hide settings button when not enabled (#47)
    * feat: hide settings button when not enabled
    
    * add TODO comment
    
    Co-authored-by: Sindre Bøyum <boyum@users.noreply.github.com>
    
    * fix TODO
    
    ---------
    
    Co-authored-by: Sindre Bøyum <boyum@users.noreply.github.com>

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -2,17 +2,23 @@ import React from "react";
 
 type ToolbarProps = {
   title: string;
+  enableSettings: boolean;
   toggleShowSettings: () => void;
 };
 
 export const Toolbar: React.FC<ToolbarProps> = ({
   title,
+  enableSettings,
   toggleShowSettings,
 }) => {
+  // TODO: Translate aria-label
+  // TODO: Open what? ref aria-label="Open"
   return (
     <div className="h5p-vocabulary-drill-toolbar">
       <p>{title}</p>
-      <button type="button" aria-label="Open" onClick={toggleShowSettings} />
+      {enableSettings && (
+        <button type="button" aria-label="Open" onClick={toggleShowSettings} />
+      )}
     </div>
   );
 };
