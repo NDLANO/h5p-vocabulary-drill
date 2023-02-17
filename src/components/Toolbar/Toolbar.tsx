5db23248099f1829ab2e9bce7f9e777285b83fb4
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2023-02-17 10:44:22 +0100

    add TODO comment
    Co-authored-by: Sindre Bøyum <boyum@users.noreply.github.com>

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -15,6 +15,8 @@ export const Toolbar: React.FC<ToolbarProps> = ({
     <div className="h5p-vocabulary-drill-toolbar">
       <p>{title}</p>
       {enableSettings && (
+        {/* TODO: Translate */ }
+        {/* TODO: Open what? */ }
         <button type="button" aria-label="Open" onClick={toggleShowSettings} />
       )}
     </div>
