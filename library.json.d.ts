394408929a85e54a767c47ee4302f16be17884c5
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2023-02-20 14:46:58 +0100

    chore: regenerate library definition

--- library.json.d.ts
+++ library.json.d.ts
@@ -1,59 +1,45 @@
-export const title : "Vocabulary Drill";
-export const machineName : "H5P.VocabularyDrill";
-export const majorVersion : 1;
-export const minorVersion : 0;
-export const patchVersion : 3;
-export const runnable : 1;
-export const license : "MIT";
-export const author : "NDLA";
-export const embedTypes : [
-	"iframe"
-];
-export const preloadedDependencies : [
-	{
-		machineName: "H5P.Blanks",
-		majorVersion: 1,
-		minorVersion: 14
-	},
-	{
-		machineName: "H5P.DragText",
-		majorVersion: 1,
-		minorVersion: 10
-	}
-];
-export const editorDependencies : [
-	{
-		machineName: "H5PEditor.CSVToText",
-		majorVersion: 1,
-		minorVersion: 0
-	},
-	{
-		machineName: "H5PEditor.RadioGroup",
-		majorVersion: 1,
-		minorVersion: 1
-	}
-];
-export const preloadedJs : [
-	{
-		path: "dist/h5p-vocabulary-drill.js"
-	},
-	{
-		path: "dist/vendor.js"
-	}
-];
-declare const $defaultExport: {
-	title: typeof title,
-	machineName: typeof machineName,
-	majorVersion: typeof majorVersion,
-	minorVersion: typeof minorVersion,
-	patchVersion: typeof patchVersion,
-	runnable: typeof runnable,
-	license: typeof license,
-	author: typeof author,
-	embedTypes: typeof embedTypes,
-	preloadedDependencies: typeof preloadedDependencies,
-	editorDependencies: typeof editorDependencies,
-	preloadedJs: typeof preloadedJs
-};
+declare const json: {
+  "title": "Vocabulary Drill",
+  "machineName": "H5P.VocabularyDrill",
+  "majorVersion": 1,
+  "minorVersion": 0,
+  "patchVersion": 3,
+  "runnable": 1,
+  "license": "MIT",
+  "author": "NDLA",
+  "embedTypes": ["iframe"],
+  "preloadedDependencies": [
+    {
+      "machineName": "H5P.Blanks",
+      "majorVersion": 1,
+      "minorVersion": 14
+    },
+    {
+      "machineName": "H5P.DragText",
+      "majorVersion": 1,
+      "minorVersion": 10
+    }
+  ],
+  "editorDependencies": [
+    {
+      "machineName": "H5PEditor.CSVToText",
+      "majorVersion": 1,
+      "minorVersion": 0
+    },
+    {
+      "machineName": "H5PEditor.RadioGroup",
+      "majorVersion": 1,
+      "minorVersion": 1
+    }
+  ],
+  "preloadedJs": [
+    {
+      "path": "dist/h5p-vocabulary-drill.js"
+    },
+    {
+      "path": "dist/vendor.js"
+    }
+  ]
+}
 
-export default $defaultExport;
+export default json;
