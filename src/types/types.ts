1ce35ed52fcfc98ccb4b001e2178f3c75a02d610
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2023-02-21 16:14:20 +0100

    refactor: sort languages alphabetically (#49)
    * refactor: sort languages alfabetically
    
    * sort languages in language files

--- src/types/types.ts
+++ src/types/types.ts
@@ -11,7 +11,6 @@ export enum LanguageModeType {
   Target = 'target',
 }
 
-
 // By using `semantics` we let `unplugin-json-dts` know that we want it to
 // generate `semantics.json.d.ts. This is a hack and should be avoided in
 // the future.
