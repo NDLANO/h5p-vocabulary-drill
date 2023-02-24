1fe0d75eef5d08575f4aee4a7a5dfc331f30b9d6
Sindre Bøyum <boyum@users.noreply.github.com>
2023-02-24 10:51:31 +0100

    chore: specify when `unplugin-json-dts` hack can be removed (#64)

--- src/types/types.ts
+++ src/types/types.ts
@@ -11,10 +11,12 @@ export enum LanguageModeType {
   Target = 'target',
 }
 
+export type Params = InferParamsFromSemantics<DeepReadonly<typeof semantics>>;
+export type TranslationKey = keyof Params['l10n'];
+
 // By using `semantics` we let `unplugin-json-dts` know that we want it to
 // generate `semantics.json.d.ts. This is a hack and should be avoided in
 // the future.
+// TODO: Remove this hack when `unplugin-json-dts` starts generating types
+// for JSON files that are imported in type scope.
 semantics;
-
-export type Params = InferParamsFromSemantics<DeepReadonly<typeof semantics>>;
-export type TranslationKey = keyof Params['l10n'];
