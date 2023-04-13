8b45dcef69fc18aee1a2fb19c1bccc179c6729da
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2023-04-13 10:09:33 +0200

    feat: use combobox

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -22,40 +22,124 @@ export const Toolbar: React.FC<ToolbarProps> = ({
   disableTools,
 }) => {
   const { t } = useTranslation();
+  const comboRef = React.useRef<HTMLDivElement>(null);
+  const option1Ref = React.useRef<HTMLDivElement>(null);
+  const option2Ref = React.useRef<HTMLDivElement>(null);
+
+  const [openMenu, setOpenMenu] = React.useState(false);
+  const [currentfocus, setCurrentFocus] = React.useState<HTMLDivElement | null>(null);
 
   const enableTools = enableAnswerMode || enableLanguageMode;
+
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu);
+    setCurrentFocus(comboRef.current);
+  };
+
+  /* handle onkeyDown */
+  const handleOpenMenuKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log(event.key);
+
+    const combofocus = currentfocus === comboRef.current;
+    const option1focus = currentfocus === option1Ref.current;
+    const option2focus = currentfocus === option2Ref.current;
+
+
+    if (!currentfocus || combofocus) {
+      if (event.key === ' ' || event.key === 'Enter') {
+        handleOpenMenu();
+        return;
+      }
+
+      if (openMenu) {
+        if (event.key === 'ArrowDown') {
+          setCurrentFocus(option1Ref.current);
+        }
+      }
+      return;
+    }
+
+    if (option1focus) {
+      if (event.key === 'ArrowDown') {
+        setCurrentFocus(option2Ref.current);
+      }
+    }
+
+    if (option2focus) {
+      if (event.key === 'ArrowUp') {
+        setCurrentFocus(option1Ref.current);
+      }
+    }
+
+  };
+
+  const handleOptionClick = (mode: AnswerModeType) => {
+    if (activeAnswerMode !== mode) {
+      onAnswerModeChange();
+    }
+    setOpenMenu(false);
+  };
 
   const classes = {
     [AnswerModeType.FillIn]: 'h5p-vocabulary-drill-fill-in',
     [AnswerModeType.DragText]: 'h5p-vocabulary-drill-drag-text',
   };
 
+  const activeModeLabel = {
+    [AnswerModeType.FillIn]: t('fillInLabel'),
+    [AnswerModeType.DragText]: t('dragTextLabel'),
+  };
+
+  const answerModes = [
+    { value: AnswerModeType.FillIn, label: t('fillInLabel') },
+    { value: AnswerModeType.DragText, label: t('dragTextLabel') },
+  ];
+
+  const comboClass = `h5p-vocabulary-drill-toolbar-select ${classes[activeAnswerMode]} ${disableTools ? 'disabled' : ''}`;
+  console.log(comboClass, answerModes);
   return (
     <div className="h5p-vocabulary-drill-toolbar">
       <p>{title}</p>
       {enableTools && (
         <div className="h5p-vocabulary-drill-toolbar-tools">
           {enableAnswerMode && (
-            <div
-              className={`h5p-vocabulary-drill-toolbar-select ${classes[activeAnswerMode]} ${disableTools ? 'disabled' : ''}`}
-            >
-              <label className="visually-hidden" htmlFor="answerMode">
+            <div>
+              <label id="answer-mode-label1" className="visually-hidden combo-label" htmlFor="answerMode">
                 {t('answerModeLabel')}
               </label>
-              <select
-                id="answerMode"
-                name="answerMode"
-                onChange={onAnswerModeChange}
-                value={activeAnswerMode}
-                disabled={disableTools}
-              >
-                <option value={AnswerModeType.FillIn}>
-                  {t('fillInLabel')}
-                </option>
-                <option value={AnswerModeType.DragText}>
-                  {t('dragTextLabel')}
-                </option>
-              </select>
+              <div className={`combo ${openMenu ? 'open' : ''}`}>
+                <div
+                  aria-controls="listbox1"
+                  aria-expanded="false"
+                  aria-haspopup="listbox"
+                  aria-labelledby="answer-mode-label1"
+                  id="combo1"
+                  className="combo-input"
+                  role="combobox"
+                  tabIndex={0}
+                  aria-activedescendant=""
+                  onClick={handleOpenMenu}
+                  onKeyDown={handleOpenMenuKeyDown}
+                  ref={comboRef}
+                >
+                  {activeModeLabel[activeAnswerMode]}
+                </div>
+                <div
+                  role="listbox"
+                  className="combo-menu"
+                  id="answer-mode-listbox1"
+                  aria-aria-labelledby="answer-mode-label1"
+                  onChange={onAnswerModeChange}
+                  tabIndex={-1}
+                >
+                  <div role="option" ref={option1Ref} id="answer-mode-combo1" className={`combo-option ${currentfocus === option1Ref.current ? "option-current" : ""}`} aria-selected={activeAnswerMode === AnswerModeType.FillIn} onClick={() => handleOptionClick(AnswerModeType.FillIn)}>
+                    {t('fillInLabel')}
+                  </div>
+                  <div role="option" ref={option2Ref} id="answer-mode-combo2" className={`combo-option ${currentfocus === option2Ref.current ? "option-current" : ""}`} aria-selected={activeAnswerMode === AnswerModeType.DragText} onClick={() => handleOptionClick(AnswerModeType.DragText)}>
+                    {t('dragTextLabel')}
+                  </div>
+                </div>
+              </div>
             </div>
           )}
           {enableLanguageMode && (
