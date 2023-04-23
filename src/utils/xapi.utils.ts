e10a0b70eeb2380d7fcb3bc4419dad82b2415358
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-04-23 17:09:13 +0200

    refactor: update h5p-types to v1.16.2 and fix remaining type errors

--- src/utils/xapi.utils.ts
+++ src/utils/xapi.utils.ts
@@ -8,24 +8,6 @@ import {
   XAPIVerb,
 } from 'h5p-types';
 
-/*
- * XAPIDefition in h5p-types is not correct: Does not require
- * `correctResponsesPattern` as it depends on interaction type
- * see https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-Data.md#interaction-activities
- */
-type XAPIDefinitionFixed = Omit<XAPIDefinition, 'correctResponsesPattern'>;
-
-// XAPIEvent in h5p-types is not correct, setScoredResult is not correct
-type XAPIEventFixed = Omit<XAPIEvent, 'setScoredResult'> & {
-  setScoredResult(
-    score: number,
-    maxScore: number,
-    instance: IH5PContentType, // H5PObject in XAPIEvent
-    completion: boolean, // number in XAPIEvent
-    success: boolean, // number in XAPIEvent
-  ): void
-}
-
 const DEFAULT_DESCRIPTION = 'Vocabulary Drill';
 
 export default class XAPIUtils {
@@ -34,9 +16,9 @@ export default class XAPIUtils {
   title?: string;
 
   constructor(params: {
-    context: IH5PContentType & IH5PQuestionType
-    description?: string,
-    title?: string
+    context: IH5PContentType & IH5PQuestionType;
+    description?: string;
+    title?: string;
   }) {
     this.context = params.context;
     this.description = params.description;
@@ -48,12 +30,12 @@ export default class XAPIUtils {
     this.context.trigger(xAPIEvent);
   }
 
-  createXAPIEvent(verb: XAPIVerb): XAPIEventFixed {
-    const xAPIEvent = this.context.createXAPIEventTemplate(verb, undefined) as unknown as XAPIEventFixed;
+  createXAPIEvent(verb: XAPIVerb): XAPIEvent {
+    const xAPIEvent = this.context.createXAPIEventTemplate(verb, undefined);
 
     xAPIEvent.data.statement.object.definition = {
       ...xAPIEvent.data.statement.object.definition,
-      ...this.getXAPIDefinition()
+      ...this.getXAPIDefinition(),
     };
 
     if (verb === 'completed' || verb === 'answered') {
@@ -62,21 +44,21 @@ export default class XAPIUtils {
         this.context.getMaxScore(),
         this.context,
         true,
-        this.context.getScore() === this.context.getMaxScore()
+        this.context.getScore() === this.context.getMaxScore(),
       );
     }
 
     return xAPIEvent;
   }
 
-  getXAPIDefinition(): XAPIDefinitionFixed {
+  getXAPIDefinition(): XAPIDefinition {
     return {
       name: { 'en-US': this.getTitle() },
       description: { 'en-US': this.getDescription() },
       type: 'http://adlnet.gov/expapi/activities/cmi.interaction',
-      interactionType: 'other' as XAPIInteractionType
+      interactionType: 'other' satisfies XAPIInteractionType,
     };
-  };
+  }
 
   getTitle(): string {
     return H5P.createTitle(this.title ?? DEFAULT_DESCRIPTION);
