2e9ebc7b60821496b6453216c321687409a8d238
Oliver Tacke <o.tacke@posteo.de>
2025-12-04 16:00:40 +0100

    Bump to 1.1.1

--- library.json.d.ts
+++ library.json.d.ts
@@ -3,7 +3,7 @@ declare const json: {
   "machineName": "H5P.VocabularyDrill",
   "majorVersion": 1,
   "minorVersion": 1,
-  "patchVersion": 0,
+  "patchVersion": 1,
   "runnable": 1,
   "license": "MIT",
   "author": "NDLA",
