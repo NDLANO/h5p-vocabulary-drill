735327b04329a150613fdd0b0affb88b856a018b
Oliver Tacke <o.tacke@posteo.de>
2025-12-04 15:59:42 +0100

    Remove flag from language selector

--- semantics.json.d.ts
+++ semantics.json.d.ts
@@ -17,6 +17,7 @@ declare const json: [
       "targetFieldMap": {
         "languageNameTranslatedContent": "sourceLanguageName"
       },
+      "noFlag": true,
       "default": "en"
     }
   },
@@ -38,6 +39,7 @@ declare const json: [
       "targetFieldMap": {
         "languageNameTranslatedContent": "targetLanguageName"
       },
+      "noFlag": true,
       "default": "nb"
     }
   },
